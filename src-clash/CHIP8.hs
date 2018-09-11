cc792ae1ac3f649705050092982746ae10cbec51
Dr. ERDI Gergo <gergo@erdi.hu>
2018-09-11 21:02:21 +0800

    Add a frame buffer, backing the VGA output

--- src-clash/CHIP8.hs
+++ src-clash/CHIP8.hs
@@ -51,12 +51,17 @@ topEntity
       )
 topEntity = exposeClockReset board
   where
-    board rxIn ps2Clk ps2Data = (txOut, (vgaVSync, vgaHSync, vgaR, vgaG, vgaB))
+    board rxIn ps2Clk ps2Data = (txOut, (delay1 high vgaVSync, delay1 high vgaHSync, vgaR, vgaG, vgaB))
       where
         txOut = pure low
 
         VGADriver{..} = vgaDriver vga640x480at60
         ps2 = decodePS2 $ samplePS2 PS2{..}
+
+        x0 = (chipX =<<) <$> vgaX
+        y0 = (chipY =<<) <$> vgaY
+
+        delay1 x = toSignal . delayed (singleton x) . fromSignal
 
         (dx, dy) = unbundle $ do
             key <- parseScanCode ps2
@@ -67,14 +72,17 @@ topEntity = exposeClockReset board
                 Just (ScanCode KeyPress 0xe074) -> (1, 0)  -- right
                 _ -> (0, 0)
 
-        pixel = do
+        fbWrite = do
             x <- fix $ register 0 . (+ dx)
             y <- fix $ register 0 . (+ dy)
-            x0 <- (chipX =<<) <$> vgaX
-            y0 <- (chipY =<<) <$> vgaY
-            pure $ case (,) <$> x0 <*> y0 of
-                Just (x0, y0) -> (x0, y0) == (x, y)
-                _ -> False
+            pure $ Just (fbIndex x y, True)
+
+        pixel = mux visible framebuf (pure False)
+          where
+            visible = isJust <$> x0 .&&. isJust <$> y0
+
+            fbRead = fbIndex <$> (fromMaybe 0 <$> x0) <*> (fromMaybe 0 <$> y0)
+            framebuf = blockRam (replicate (SNat :: SNat 2048) False) fbRead fbWrite
 
         vgaR = monochrome <$> pixel
         vgaG = monochrome <$> pixel
@@ -93,3 +101,6 @@ chipY y = let (y', _) = unpack . pack $ y :: (Unsigned 7, Unsigned 3)
 
 serialRate :: Word32
 serialRate = 9600
+
+fbIndex :: VidX -> VidY -> Unsigned 11
+fbIndex x y = unpack . pack $ (x, y)
