4c2362dae28d2386a25fff6e0eeccf43691745c5
Dr. ERDI Gergo <gergo@erdi.hu>
2018-09-13 09:27:39 +0800

    Fill in implementation of virtual keypad

--- src-clash/CHIP8/Keypad.hs
+++ src-clash/CHIP8/Keypad.hs
@@ -12,21 +12,41 @@ import Data.Word
 import Data.Maybe (fromMaybe, isJust, fromJust)
 import Control.Monad (guard)
 import Data.Traversable (forM)
+import Data.Foldable (find)
 
 keypad
     :: (HiddenClockReset dom gated synchronous)
     => Signal dom (Maybe ScanCode) -> (Signal dom KeypadState, Signal dom (Maybe (KeyEvent, Key)))
-keypad rawEvent = (keys, event)
+keypad scanCode = (keys, event)
   where
-    event = do
-        rawEvent <- rawEvent
-        -- pure $ forM rawEvent $ \(ScanCode ev ext code) -> case (ext, code) of
-        pure undefined
+    event = (fromScanCode =<<) <$> scanCode
 
     keys = regMaybe (pure False) $ do
         ev <- event
         keys <- keys
-        return $ applyEvent <$> ev <*> pure keys
+        pure $ flip applyEvent keys <$> ev
 
 applyEvent :: (KeyEvent, Key) -> KeypadState -> KeypadState
 applyEvent (ev, key) = replace key (ev == KeyPress)
+
+fromScanCode :: ScanCode -> Maybe (KeyEvent, Key)
+fromScanCode (ScanCode ev ext code) = do
+    guard $ ext == False
+    (_, key) <- find ((code ==) . fst) (zip (concat codes) (concat layout))
+    pure (ev, key)
+
+layout :: Vec 4 (Vec 4 Key)
+layout =
+    (0x1 :> 0x2 :> 0x3 :> 0xc :> Nil) :>
+    (0x4 :> 0x5 :> 0x6 :> 0xd :> Nil) :>
+    (0x7 :> 0x8 :> 0x9 :> 0xe :> Nil) :>
+    (0xa :> 0x0 :> 0xb :> 0xf :> Nil) :>
+    Nil
+
+codes :: Vec 4 (Vec 4 Word8)
+codes =
+    (0x16 :> 0x1e :> 0x26 :> 0x25 :> Nil) :>
+    (0x15 :> 0x1d :> 0x24 :> 0x2d :> Nil) :>
+    (0x1c :> 0x1b :> 0x23 :> 0x2b :> Nil) :>
+    (0x1a :> 0x22 :> 0x21 :> 0x2a :> Nil) :>
+    Nil
