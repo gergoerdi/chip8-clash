66ea255efe7d0cb9089e4b3fa539ad7c9aa019a6
Dr. ERDI Gergo <gergo@erdi.hu>
2019-02-09 15:37:42 +0800

    Import trimmed-down version of Terasic HDMI demo wholesale

---
+++ src-vhdl/de10-nano/TopWrapper.v
@@ -0,0 +1,51 @@
+module TopWrapper(
+
+      ///////// FPGA /////////
+      input              FPGA_CLK1_50,
+
+      ///////// HDMI /////////
+      inout              HDMI_I2C_SCL,
+      inout              HDMI_I2C_SDA,
+      inout              HDMI_I2S,
+      inout              HDMI_LRCLK,
+      inout              HDMI_MCLK,
+      inout              HDMI_SCLK,
+      output             HDMI_TX_CLK,
+      output      [23:0] HDMI_TX_D,
+      output             HDMI_TX_DE,
+      output             HDMI_TX_HS,
+      input              HDMI_TX_INT,
+      output             HDMI_TX_VS
+);
+
+
+
+wire				reset_n;
+//Video Pattern Generator
+wire	[3:0]		vpg_disp_mode;
+wire				vpg_pclk;
+wire				vpg_de, vpg_hs, vpg_vs;
+wire	[23:0]	vpg_data;
+
+I2C_HDMI_Config u_I2C_HDMI_Config (
+	.iCLK(FPGA_CLK1_50),
+	.iRST_N(reset_n),
+	.I2C_SCLK(HDMI_I2C_SCL),
+	.I2C_SDAT(HDMI_I2C_SDA),
+	.HDMI_TX_INT(HDMI_TX_INT)
+	 );
+
+assign reset_n = 1'b1;
+
+vpg u_vpg (
+	.clk_50(FPGA_CLK1_50),
+	.reset_n(reset_n),
+	.vpg_pclk(HDMI_TX_CLK),
+	.vpg_de(HDMI_TX_DE),
+	.vpg_hs(HDMI_TX_HS),
+	.vpg_vs(HDMI_TX_VS),
+	.vpg_r(HDMI_TX_D[23:16]),
+	.vpg_g(HDMI_TX_D[15:8]),
+	.vpg_b(HDMI_TX_D[7:0]) );
+
+endmodule
